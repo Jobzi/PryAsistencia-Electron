609a352e4f166fc5e1c4aef30d931abcbb7f1422
Daniel <danielpaqueuieressabereso@jaja.saludos>
2019-11-18 23:44:43 -0500

    70% funcional corregir boostrap

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-const { app, BrowserWindow, Menu, ipcMain } = require('electron');
-
+const { app, BrowserWindow, Menu } = require('electron');
+const mysql = require("mysql");
 const url = require('url');
 const path = require('path');
-
+exports.Menu=Menu;
 let mainWindow;
-let newProductWindow;
+
+const connection = mysql.createConnection({
+  host:"remotemysql.com",
+  port:"3306",
+  user:"V4m8MKkMx1",
+  password:"qSE61lK4af",
+  database:"V4m8MKkMx1",
+}) 
+connection.connect(function(err){
+  if (err){
+    console.log(err.code)
+  }
+})
+exports.connection=connection
+exports.closeConnection = () =>{
+  connection.end(function(){})
+}
+
+
 
 // Reload in Development for Browser Windows
 if(process.env.NODE_ENV !== 'production') {
@@ -13,85 +31,35 @@ if(process.env.NODE_ENV !== 'production') {
   });
 }
 
-
 app.on('ready', () => {
 
   // The Main Window
   mainWindow = new BrowserWindow({width: 1000, height: 750});
-
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'views/index.html'),
     protocol: 'file',
     slashes: true
   }))
 
-  // Menu
-  const mainMenu = Menu.buildFromTemplate(templateMenu);
+
   // Set The Menu to the Main Window
-  Menu.setApplicationMenu(mainMenu);
+  Menu.setApplicationMenu(null);
 
   // If we close main Window the App quit
   mainWindow.on('closed', () => {
     app.quit();
   });
-
 });
-
-
-function createNewProductWindow() {
-  newProductWindow = new BrowserWindow({
-    width: 400,
-    height: 330,
-    title: 'Add A New Product'
-  });
-  newProductWindow.setMenu(null);
-
-  newProductWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'views/new-product.html'),
-    protocol: 'file',
-    slashes: true
-  }));
-  newProductWindow.on('closed', () => {
-    newProductWindow = null;
-  });
-}
-
-// Ipc Renderer Events
-ipcMain.on('product:new', (e, newProduct) => {
-  // send to the Main Window
-  console.log(newProduct);
-  mainWindow.webContents.send('product:new', newProduct);
-  newProductWindow.close();
-});
-
 
 // Menu Template
 const templateMenu = [
   {
-    label: 'File',
-    submenu: [
-      {
-        label: 'New Product',
-        accelerator: 'Ctrl+N',
-        click() {
-          createNewProductWindow();
-        }
-      },
-      {
-        label: 'Remove All Products',
-        click() {
-          mainWindow.webContents.send('products:remove-all');
-        }
-      },
-      {
-        label: 'Exit',
-        accelerator: process.platform == 'darwin' ? 'command+Q' : 'Ctrl+Q',
-        click() {
-          app.quit();
-        }
-      }
-    ]
-  }
+    label: 'reload',
+    click() {
+          console.log('7:00:00');
+          
+    }
+  },
 ];
 
 // if you are in Mac, just add the Name of the App
---
+++ src/views/js/funciones.js
@@ -0,0 +1,437 @@
+const remote = require('electron').remote
+const index = remote.require('./index.js')
+const fecha=new Date();
+const hora_actual=fecha.getHours()+":"+fecha.getMinutes()+":"+"00";
+
+function listarControles(horario) {
+    //saber hora para el selecet
+    if (horario==""){        
+        horario=obtenerHoraLaboral();
+    }
+    if(horario!="No es Hora Laboral"){
+      dibujarSalas()
+    }
+    saberDia(horario)
+    let hora=horario.split("-",1)
+    //
+    let string_fecha=fecha.getFullYear() + "-" + (fecha.getMonth() +1) + "-" + fecha.getDate();
+    console.log(string_fecha)
+    let query = 'select CON_DIA,MAT_ABREVIATURA,MAT_NRC,DOC_NOMBRES,DOC_APELLIDOS,DOC_TITULO,CON_EXTRA,LAB_NOMBRE,control.CON_HORA_ENTRADA,control.CON_HORA_SALIDA,control.CON_HORA_ENTRADA_R,control.CON_HORA_SALIDA_R FROM control,materia,docente,laboratorio WHERE control.MAT_CODIGO=materia.MAT_CODIGO and control.DOC_CODIGO=docente.DOC_CODIGO and control.LAB_CODIGO=laboratorio.LAB_CODIGO and control.CON_DIA="2019-11-22" and control.CON_HORA_ENTRADA='+'"'+hora+'"'
+    let salas=["SALA 01","SALA 02","SALA 03","SALA 04","SALA 05","SALA 06","COMPUTACIÓN I","COMPUTACIÓN II","REDES DE DATOS"];
+    let sala_nombre;
+    let ocasional="H";
+    let color_tarjeta=["light","warning","danger","success"];
+    let accion_boton=["DISPONIBLE","ENTRAR","SALIR","FINALIZADO"]
+    let indice_colores;
+    index.connection.query(query,function(err,rows,fields){
+        if (err){
+          console.log("error fatal")
+          console.log(err)
+          
+          return
+        }
+
+        controles=rows;
+        controles.forEach(control => {
+            //Obteniendo el codigo de la sala
+            sala_nombre=control.LAB_NOMBRE.split(" ", 2);
+            if (sala_nombre[0]=="SALA"){
+                sala_codigo="S"+sala_nombre[1].split("0",2)[1];
+            }else{
+                if(sala_nombre[0]=="COMPUTACIÓN"){
+                  if(sala_nombre[1]=="I"){
+                    sala_codigo="C"+"1";
+                  }else{
+                    sala_codigo="C"+"2";
+                  }
+                }else{
+                    sala_codigo="RE";
+                }
+            }
+            //Obtienedo si es ocacional
+            if (control.CON_EXTRA==1){
+                ocasional="O"
+            }
+            //Obteniendo el estado de control
+            if(control.CON_HORA_ENTRADA_R!=null){
+              if(control.CON_HORA_SALIDA_R==null){
+                indice_colores=2;
+              }else{
+                indice_colores=3
+              }  
+            }else{
+              indice_colores=1;
+            }
+            const controlesTemplate = `
+            <div id= ${sala_nombre}>
+            <div class="card text-white bg-${color_tarjeta[indice_colores]} mb-5" style="max-width: 20rem;">
+                <div class="card-header">
+                    <div class="row">
+                    <div class="col-md-2">
+                        <label  class="btn btn-secondary">${sala_codigo}</label>
+                    </div>
+                    <div class="col-md-8">
+                        <h5>${control.MAT_ABREVIATURA} </h5>
+                    </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <h4 class="card-title">${control.DOC_TITULO} ${control.DOC_APELLIDOS} ${control.DOC_NOMBRES} <a class="card-text" style="color:#fffb00";>${ocasional}</a> </h4>
+                </div>
+                <div class="card-footer">
+                    <button class="btn btn-${color_tarjeta[indice_colores]}">
+                      ${accion_boton[indice_colores]}
+                    </button>
+                </div>
+                </div>
+            </div>
+            `;
+            salas.forEach(sala=>{
+                if(control.LAB_NOMBRE==sala){
+                    document.getElementById(sala).innerHTML = controlesTemplate;
+                }
+            })
+      })
+    })
+
+    //index.closeConnection()
+}
+
+function dibujarSalas(){
+    var salasTemplate =`
+    <div class="row" id="salas">    
+        <div class="col-xs-4 p-2" id="SALA 01">
+            <div class="card  bg-light mb-5" style="max-width: 20rem;" >
+              <div class="card-header">
+                <div class="row">
+                  <div class="col-md-2">
+                      <label  class="btn btn-secondary">S1</label>
+                  </div>
+                  <div class="col-md-8">
+                    <h5></h5>
+                  </div>
+                </div>
+              </div>
+              <div class="card-body">
+                <h4 class="card-title"></h4>
+              </div>
+              <div class="card-footer">
+                  <button class="btn btn-secondary">
+                    DISPONIBLE
+                  </button>
+              </div>
+            </div>
+          </div>  
+        <!---->
+        <div class="col-xs-4 p-2" id="SALA 02">
+            <div class="card  bg-light mb-5" style="max-width: 20rem;" >
+              <div class="card-header">
+                <div class="row">
+                  <div class="col-md-2">
+                      <label  class="btn btn-secondary">S2</label>
+                  </div>
+                  <div class="col-md-8">
+                    <h5></h5>
+                  </div>
+                </div>
+              </div>
+              <div class="card-body">
+                <h4 class="card-title"></h4>
+              </div>
+              <div class="card-footer">
+                  <button class="btn btn-secondary">
+                    DISPONIBLE
+                  </button>
+              </div>
+            </div>
+          </div>  
+        <!---->
+        <div class="col-xs-4 p-2" id="SALA 03">
+            <div class="card  bg-light mb-5" style="max-width: 20rem;" >
+              <div class="card-header">
+                <div class="row">
+                  <div class="col-md-2">
+                      <label  class="btn btn-secondary">S3</label>
+                  </div>
+                  <div class="col-md-8">
+                    <h5></h5>
+                  </div>
+                </div>
+              </div>
+              <div class="card-body">
+                <h4 class="card-title"></h4>
+              </div>
+              <div class="card-footer">
+                  <button class="btn btn-secondary">
+                    DISPONIBLE
+                  </button>
+              </div>
+            </div>
+          </div>  
+      <!---->
+        <div class="col-xs-4 p-2" id="SALA 04">
+          <div class="card  bg-light mb-5" style="max-width: 20rem;" >
+            <div class="card-header">
+              <div class="row">
+                <div class="col-md-2">
+                    <label  class="btn btn-secondary">S4</label>
+                </div>
+                <div class="col-md-8">
+                  <h5></h5>
+                </div>
+              </div>
+            </div>
+            <div class="card-body">
+              <h4 class="card-title"></h4>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-secondary">
+                  DISPONIBLE
+                </button>
+            </div>
+          </div>
+        </div>    
+       <!---->
+       <div class="col-xs-4 p-2" id="SALA 05">
+          <div class="card  bg-light mb-5" style="max-width: 20rem;" >
+            <div class="card-header">
+              <div class="row">
+                <div class="col-md-2">
+                    <label  class="btn btn-secondary">S5</label>
+                </div>
+                <div class="col-md-8">
+                  <h5></h5>
+                </div>
+              </div>
+            </div>
+            <div class="card-body">
+              <h4 class="card-title"></h4>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-secondary">
+                  DISPONIBLE
+                </button>
+            </div>
+          </div>
+        </div>    
+       <!---->
+       <div class="col-xs-4 p-2" id="SALA 06">
+          <div class="card  bg-light mb-5" style="max-width: 20rem;" >
+            <div class="card-header">
+              <div class="row">
+                <div class="col-md-2">
+                    <label  class="btn btn-secondary">S6</label>
+                </div>
+                <div class="col-md-8">
+                  <h5></h5>
+                </div>
+              </div>
+            </div>
+            <div class="card-body">
+              <h4 class="card-title"></h4>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-secondary">
+                  DISPONIBLE
+                </button>
+            </div>
+          </div>
+        </div>    
+       <!---->
+       <div class="col-xs-4 p-2" id="REDES DE DATOS">
+          <div class="card  bg-light mb-5" style="max-width: 20rem;" >
+            <div class="card-header">
+              <div class="row">
+                <div class="col-md-2">
+                    <label  class="btn btn-secondary">RE</label>
+                </div>
+                <div class="col-md-8">
+                  <h5></h5>
+                </div>
+              </div>
+            </div>
+            <div class="card-body">
+              <h4 class="card-title"></h4>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-secondary">
+                  DISPONIBLE
+                </button>
+            </div>
+          </div>
+        </div>    
+       <!---->
+       <div class="col-xs-4 p-2" id="COMPUTACIÓN I">
+          <div class="card  bg-light mb-5" style="max-width: 20rem;" >
+            <div class="card-header">
+              <div class="row">
+                <div class="col-md-2">
+                    <label  class="btn btn-secondary">C1</label>
+                </div>
+                <div class="col-md-8">
+                  <h5></h5>
+                </div>
+              </div>
+            </div>
+            <div class="card-body">
+              <h4 class="card-title"></h4>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-secondary">
+                  DISPONIBLE
+                </button>
+            </div>
+          </div>
+        </div>    
+       <!---->
+       <div class="col-xs-4 p-2" id="COMPUTACION II">
+          <div class="card  bg-light mb-5" style="max-width: 20rem;" >
+            <div class="card-header">
+              <div class="row">
+                <div class="col-md-2">
+                    <label  class="btn btn-secondary">C2</label>
+                </div>
+                <div class="col-md-8">
+                  <h5></h5>
+                </div>
+              </div>
+            </div>
+            <div class="card-body">
+              <h4 class="card-title"></h4>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-secondary">
+                  DISPONIBLE
+                </button>
+            </div>
+          </div>
+        </div>
+    </div>    
+        `;
+      document.getElementById("salas").innerHTML = salasTemplate
+}
+
+function reloj(){
+    if (!document.layers&&!document.all&&!document.getElementById)
+    return
+    
+     const Digital=new Date()
+     var hours=Digital.getHours()
+     var minutes=Digital.getMinutes()
+     var seconds=Digital.getSeconds()
+    
+    var dn="PM"
+    if (hours<12)
+    dn="AM"
+    if (hours>12)
+    hours=hours-12
+    if (hours==0)
+    hours=12
+    
+     if (minutes<=9)
+     minutes="0"+minutes
+     if (seconds<=9)
+     seconds="0"+seconds
+    //change font size here to your desire
+    myclock=hours+":"+minutes+":"
+     +seconds+" "+dn+"</b></font>"
+    if (document.layers){
+    document.layers.liveclock.document.write(myclock)
+    document.layers.liveclock.document.close()
+    }
+    else if (document.all)
+    liveclock.innerHTML=myclock
+    else if (document.getElementById)
+    document.getElementById("liveclock").innerHTML=myclock
+    setTimeout("reloj()",1000)
+    }
+function saberDia(horario){
+    let diasSemana = new Array("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado");
+    let dia_actual = diasSemana[fecha.getDay()];
+    let diaTemplate =` 
+    <div id=dia>
+        <h1>${dia_actual}: ${horario}</h1>
+    </div>
+    `
+    document.getElementById("dia").innerHTML = diaTemplate
+}
+function obtenerHoraLaboral(){
+    var hora="No es Hora Laboral";
+    if(hora_actual.valueOf()>=("06:45:00").valueOf() && hora_actual.valueOf()<("09:15:00").valueOf()){
+        hora="07:00:00-09:00:00"
+    }else{
+        if(hora_actual.valueOf()>=("09:15:00").valueOf() && hora_actual.valueOf()<("11:45:00").valueOf()){
+            hora="09:30:00-11:30:00"
+        }else{
+            if(hora_actual.valueOf()>=("11:45:00").valueOf() && hora_actual.valueOf()<("14:00:00").valueOf()){
+                hora="12:00:00-14:00:00"
+            }else{
+                if(hora_actual.valueOf()>=("14:00:00").valueOf() && hora_actual.valueOf()<("16:15:00").valueOf()){
+                    hora="14:00:00-16:00:00"
+                }
+            }
+        }
+    }
+    return hora 
+}
+
+function menubar(){
+  const templateMenu = [
+    {
+      label: '7:00-9:00',
+      click() {
+        if(hora_actual.valueOf()>="06:45:00"){
+          listarControles("07:00:00-9:00:00");
+        }else{
+          alert("No puede acceder a controles futuros");
+        }
+      }
+    },
+    {
+      label: '9:30-11:30',
+      click() {
+        if(hora_actual.valueOf()>="09:15:00"){
+          listarControles("09:30:00-11:30:00");
+        }else{
+          alert("No puede acceder a controles futuros");
+        }
+      }
+    },
+    {
+      label: '12:00-14:00',
+      click() {
+        if(hora_actual.valueOf()>="11:45:00"){
+          listarControles("12:00:00-14:00:00");
+        }else{
+          alert("No puede acceder a controles futuros");
+        }
+      }
+    },
+    {
+      label: '14:00-16:00',
+      click() {
+        if(hora_actual.valueOf()>="14:00:00"){
+          listarControles("14:00:00-16:00:00");
+        }else{
+          alert("No puede acceder a controles futuros"+hora_actual);
+        }
+      }
+    },  
+  ];
+    // Menu
+    const mainMenu = index.Menu.buildFromTemplate(templateMenu);
+    // Set The Menu to the Main Window
+    index.Menu.setApplicationMenu(mainMenu);
+    // Menu Template
+
+}
+
+
+
+function iniciar(){
+    listarControles("");
+    reloj();
+    menubar();
+    
+}
+window.onload=iniciar;
