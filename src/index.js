4a3b0ea92321a8bcacdad3298fb23617b6568046
Daniel <danielpaqueuieressabereso@jaja.saludos>
2019-11-23 22:02:54 -0500

    Cambios del monta sin bootstrap (falta el refresh automatico)

--- src/index.js
+++ src/index.js
@@ -8,16 +8,16 @@ exports.path=path;
 var mainWindow;
 
 const bd_connection_info={
-  /* host:"remotemysql.com",
+   host:"remotemysql.com",
    port:"3306",
-   user:"V4m8MKkMx1",
-   password:"qSE61lK4af",
-   database:"V4m8MKkMx1",*/
-   host:"10.2.0.3",
+   user:"ILq1OZnSWd",
+   password:"Qa5qZmykgU",
+   database:"ILq1OZnSWd",
+   /*host:"10.2.0.3",
    port:"3306",
    user:"sg-lab2",
    password:"sglab2019",
-   database:"sg-lab2",
+   database:"sg-lab2",*/
  }
 
 
--- src/views/js/funciones.js
+++ src/views/js/funciones.js
@@ -1,6 +1,7 @@
 const remote = require('electron').remote
 const index = remote.require('./index.js')
 const mysql = require("mysql");
+const credenciales=index.bd_connection_info;
 const fecha=new Date();
 var horario_actual;
 var hora_actual=fecha.getHours()+":"+fecha.getMinutes()+":"+"00";
@@ -8,33 +9,25 @@ if(fecha.getHours()<10){
   var hora_actual="0"+fecha.getHours()+":"+fecha.getMinutes()+":"+"00";
 }
 function listarControles(horario) {
-  let salas=["SALA 01","SALA 02","SALA 03","SALA 04","SALA 05","SALA 06","COMPUTACIÓN I","COMPUTACIÓN II","REDES DE DATOS"];
     //saber hora para el selecet
     if (horario==""){        
         horario=obtenerHoraLaboral();
     }
     if(horario!="No es Hora Laboral"){
       dibujarSalas()
-      salas.forEach(sala=>{
-        mostrarLaboratorios("C-"+sala)
-      })
     }
     horario_actual=horario;
     saberDia(horario)
     let hora=horario.split("-",1)
     //
-    let string_fecha=fecha.getFullYear() + "-" + (fecha.getMonth() +1) + "-" + fecha.getDate();
+    //let string_fecha=fecha.getFullYear() + "-" + (fecha.getMonth() +1) + "-" + fecha.getDate();
+    let string_fecha='2015-01-07'
     console.log(string_fecha)
-    let query = 'select CON_DIA,MAT_ABREVIATURA,control.DOC_CODIGO,DOC_NOMBRES,DOC_APELLIDOS,DOC_TITULO,CON_EXTRA,LAB_NOMBRE,control.CON_HORA_ENTRADA,control.CON_HORA_SALIDA,control.CON_HORA_ENTRADA_R,control.CON_HORA_SALIDA_R FROM control,materia,docente,laboratorio WHERE control.MAT_CODIGO=materia.MAT_CODIGO and control.DOC_CODIGO=docente.DOC_CODIGO and control.LAB_CODIGO=laboratorio.LAB_CODIGO and control.CON_DIA="'+string_fecha+'" and control.CON_HORA_ENTRADA='+'"'+hora+'"'
-    let sala_nombre;
-    let ocasional="H";
+    let query = 'select CON_DIA,MAT_ABREVIATURA,control.DOC_CODIGO,DOC_NOMBRES,DOC_APELLIDOS,DOC_TITULO,CON_EXTRA,LAB_ABREVIATURA,LAB_ESTADO,control.CON_HORA_ENTRADA,control.CON_HORA_SALIDA,control.CON_HORA_ENTRADA_R,control.CON_HORA_SALIDA_R FROM control,materia,docente,laboratorio WHERE control.MAT_CODIGO=materia.MAT_CODIGO and control.DOC_CODIGO=docente.DOC_CODIGO and control.LAB_CODIGO=laboratorio.LAB_CODIGO and control.CON_DIA="'+string_fecha+'" and control.CON_HORA_ENTRADA='+'"'+hora+'"'
     let color_tarjeta=["light","danger","warning","success"];
     let accion_boton=["DISPONIBLE","ENTRAR","SALIR","FINALIZADO"]
     let indice_colores;
-    const credenciales=index.bd_connection_info;
     const connection = mysql.createConnection(credenciales)
-    //mostrar las salas marcadas
-    //
     connection.connect() 
     connection.query(query,function(err,rows,fields){
         if (err){
@@ -44,82 +37,77 @@ function listarControles(horario) {
         }
         controles=rows;
         controles.forEach(control => {
-            //Obteniendo el codigo de la sala
-            sala_nombre=control.LAB_NOMBRE.split(" ", 2);
-            if (sala_nombre[0]=="SALA"){
-                sala_codigo="S"+sala_nombre[1].split("0",2)[1];
-            }else{
-                if(sala_nombre[0]=="COMPUTACIÓN"){
-                  if(sala_nombre[1]=="I"){
-                    sala_codigo="C"+"1";
-                  }else{
-                    sala_codigo="C"+"2";
-                  }
+            if(control.LAB_ESTADO==1){
+              //Obtienedo si es ocacional
+              let ocasional="H";
+              if (control.CON_EXTRA==1){
+                  ocasional="O"
+              }
+              //Obteniendo el estado de control
+              if(control.CON_HORA_ENTRADA_R!=null){
+                if(control.CON_HORA_SALIDA_R==null){
+                  indice_colores=2;
                 }else{
-                    sala_codigo="RE";
-                }
-            }
-            //Obtienedo si es ocacional
-            if (control.CON_EXTRA==1){
-                ocasional="O"
-            }
-            //Obteniendo el estado de control
-            if(control.CON_HORA_ENTRADA_R!=null){
-              if(control.CON_HORA_SALIDA_R==null){
-                indice_colores=2;
+                  indice_colores=3
+                }  
               }else{
-                indice_colores=3
-              }  
-            }else{
-              indice_colores=1;
-            }
-            const controlesTemplate = `
-            <div id= ${sala_nombre}>
-            <div class="card text-white bg-${color_tarjeta[indice_colores]} mb-5" style="max-width: 20rem;">
-                <div class="card-header">
-                    <div class="row">
-                    <div class="col-md-2">
-                        <label  class="btn btn-primary">${sala_codigo}</label>
-                    </div>
-                    <div class="col-md-8">
-                        <h5>${control.MAT_ABREVIATURA} </h5>
-                    </div>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <h4 class="card-title">${control.DOC_TITULO} ${control.DOC_APELLIDOS} ${control.DOC_NOMBRES} <a class="card-text" style="color:#fffb00";>${ocasional}</a> </h4>
-                </div>
-                <div class="card-footer">
-                    <button id=boton${sala_codigo} class="btn btn-${color_tarjeta[indice_colores]} btn-block">
-                      ${accion_boton[indice_colores]}
-                    </button>
-                </div>
-                </div>
-            </div>
-            `;
-            salas.forEach(sala=>{
-                if(control.LAB_NOMBRE==sala){
-                    document.getElementById(sala).innerHTML = controlesTemplate;
-                }
-            })
-            let btn=document.getElementById("boton"+sala_codigo)
-            btn.addEventListener('click', e => {  
-              crearVentanaAutentificar(control.DOC_CODIGO, (btn.textContent).trim())
-            })
+                indice_colores=1;
+              }
+              const controlesTemplate = `
+              <div id= ${control.LAB_ABREVIATURA}>
+              <div class="card text-white bg-${color_tarjeta[indice_colores]} mb-5" style="max-width: 20rem;">
+                  <div class="card-header">
+                      <div class="row">
+                      <div class="col-md-2">
+                          <label  class="btn btn-primary">${control.LAB_ABREVIATURA}</label>
+                      </div>
+                      <div class="col-md-8">
+                          <h5>${control.MAT_ABREVIATURA} </h5>
+                      </div>
+                      </div>
+                  </div>
+                  <div class="card-body">
+                      <h4 class="card-title">${control.DOC_TITULO} ${control.DOC_APELLIDOS} ${control.DOC_NOMBRES} <a class="card-text" style="color:#fffb00";>${ocasional}</a> </h4>
+                  </div>
+                  <div class="card-footer">
+                      <button id=boton${control.LAB_ABREVIATURA} class="btn btn-${color_tarjeta[indice_colores]} btn-block">
+                        ${accion_boton[indice_colores]}
+                      </button>
+                  </div>
+                  </div>
+              </div>
+              `;
+              document.getElementById(control.LAB_ABREVIATURA).innerHTML = controlesTemplate;
+              let btn=document.getElementById("boton"+control.LAB_ABREVIATURA)
+              btn.addEventListener('click', e => {  
+                crearVentanaAutentificar(control.DOC_CODIGO, (btn.textContent).trim())
+              })
+          }
       })
     })
     connection.end()
 }
 
 function dibujarSalas(){
-    var salasTemplate =`
-    <div class="row" id="salas">    
-        <div class="col-xs-4 p-2" id="SALA 01" style="display: none">
+    const connection = mysql.createConnection(credenciales)
+    var salasTemplate='<div class="row" id="salas">'
+    let query = 'select LAB_ABREVIATURA, CAM_CODIGO FROM laboratorio WHERE LAB_ESTADO=1'
+    connection.connect() 
+    connection.query(query,function(err,rows,fields){
+        if (err){
+          console.log("error fatal")
+          console.log(err)
+          return
+        }
+        let salas=rows;
+        salas.forEach(sala => {
+          salasTemplate+=`
+          <div class="col-xs-4 p-2" id="${sala.LAB_ABREVIATURA}" style="display: none">
             <div class="card  bg-light mb-5" style="max-width: 20rem;" >
               <div class="card-header">
                 <div class="row">
                   <div class="col-md-2">
-                      <label  class="btn btn-primary">S1</label>
+                      <label  class="btn btn-primary">${sala.LAB_ABREVIATURA}</label>
                   </div>
                   <div class="col-md-8">
                     <h5></h5>
@@ -136,197 +124,23 @@ function dibujarSalas(){
               </div>
             </div>
           </div>  
-        <!---->
-        <div class="col-xs-4 p-2" id="SALA 02" style="display: none">
-            <div class="card  bg-light mb-5" style="max-width: 20rem;" >
-              <div class="card-header">
-                <div class="row">
-                  <div class="col-md-2">
-                      <label  class="btn btn-primary">S2</label>
-                  </div>
-                  <div class="col-md-8">
-                    <h5></h5>
-                  </div>
-                </div>
-              </div>
-              <div class="card-body">
-                <h4 class="card-title"></h4>
-              </div>
-              <div class="card-footer">
-                  <button class="btn btn-secondary btn-block">
-                    DISPONIBLE
-                  </button>
-              </div>
-            </div>
-          </div>  
-        <!---->
-        <div class="col-xs-4 p-2" id="SALA 03" style="display: none">
-            <div class="card  bg-light mb-5" style="max-width: 20rem;" >
-              <div class="card-header">
-                <div class="row">
-                  <div class="col-md-2">
-                      <label  class="btn btn-primary">S3</label>
-                  </div>
-                  <div class="col-md-8">
-                    <h5></h5>
-                  </div>
-                </div>
-              </div>
-              <div class="card-body">
-                <h4 class="card-title"></h4>
-              </div>
-              <div class="card-footer">
-                  <button class="btn btn-secondary btn-block">
-                    DISPONIBLE
-                  </button>
-              </div>
-            </div>
-          </div>  
-      <!---->
-        <div class="col-xs-4 p-2" id="SALA 04" style="display: none">
-          <div class="card  bg-light mb-5" style="max-width: 20rem;" >
-            <div class="card-header">
-              <div class="row">
-                <div class="col-md-2">
-                    <label  class="btn btn-primary">S4</label>
-                </div>
-                <div class="col-md-8">
-                  <h5></h5>
-                </div>
-              </div>
-            </div>
-            <div class="card-body">
-              <h4 class="card-title"></h4>
-            </div>
-            <div class="card-footer">
-                <button class="btn btn-secondary btn-block">
-                  DISPONIBLE
-                </button>
-            </div>
-          </div>
-        </div>    
-       <!---->
-       <div class="col-xs-4 p-2" id="SALA 05" style="display: none">
-          <div class="card  bg-light mb-5" style="max-width: 20rem;" >
-            <div class="card-header">
-              <div class="row">
-                <div class="col-md-2">
-                    <label  class="btn btn-primary">S5</label>
-                </div>
-                <div class="col-md-8">
-                  <h5></h5>
-                </div>
-              </div>
-            </div>
-            <div class="card-body">
-              <h4 class="card-title"></h4>
-            </div>
-            <div class="card-footer">
-                <button class="btn btn-secondary btn-block">
-                  DISPONIBLE
-                </button>
-            </div>
-          </div>
-        </div>    
-       <!---->
-       <div class="col-xs-4 p-2" id="SALA 06" style="display: none">
-          <div class="card  bg-light mb-5" style="max-width: 20rem;" >
-            <div class="card-header">
-              <div class="row">
-                <div class="col-md-2">
-                    <label  class="btn btn-primary ">S6</label>
-                </div>
-                <div class="col-md-8">
-                  <h5></h5>
-                </div>
-              </div>
-            </div>
-            <div class="card-body">
-              <h4 class="card-title"></h4>
-            </div>
-            <div class="card-footer">
-                <button class="btn btn-secondary btn-block">
-                  DISPONIBLE
-                </button>
-            </div>
-          </div>
-        </div>    
-       <!---->
-       <div class="col-xs-4 p-2" id="REDES DE DATOS" style="display: none">
-          <div class="card  bg-light mb-5" style="max-width: 20rem;" >
-            <div class="card-header">
-              <div class="row">
-                <div class="col-md-2">
-                    <label  class="btn btn-primary">RE</label>
-                </div>
-                <div class="col-md-8">
-                  <h5></h5>
-                </div>
-              </div>
-            </div>
-            <div class="card-body">
-              <h4 class="card-title"></h4>
-            </div>
-            <div class="card-footer">
-                <button class="btn btn-secondary btn-block">
-                  DISPONIBLE
-                </button>
-            </div>
-          </div>
-        </div>    
-       <!---->
-       <div class="col-xs-4 p-2" id="COMPUTACIÓN I" style="display: none">
-          <div class="card  bg-light mb-5" style="max-width: 20rem;" >
-            <div class="card-header">
-              <div class="row">
-                <div class="col-md-2">
-                    <label  class="btn btn-primary">C1</label>
-                </div>
-                <div class="col-md-8">
-                  <h5></h5>
-                </div>
-              </div>
-            </div>
-            <div class="card-body">
-              <h4 class="card-title"></h4>
-            </div>
-            <div class="card-footer">
-                <button class="btn btn-secondary btn-block">
-                  DISPONIBLE
-                </button>
-            </div>
-          </div>
-        </div>    
-       <!---->
-       <div class="col-xs-4 p-2" id="COMPUTACIÓN II" style="display: none">
-          <div class="card  bg-light mb-5" style="max-width: 20rem;" >
-            <div class="card-header">
-              <div class="row">
-                <div class="col-md-2">
-                    <label  class="btn btn-primary">C2</label>
-                </div>
-                <div class="col-md-8">
-                  <h5></h5>
-                </div>
-              </div>
-            </div>
-            <div class="card-body">
-              <h4 class="card-title"></h4>
-            </div>
-            <div class="card-footer">
-                <button class="btn btn-secondary btn-block">
-                  DISPONIBLE
-                </button>
-            </div>
-          </div>
-        </div>
-    </div>    
-        `;
-      document.getElementById("salas").innerHTML = salasTemplate
+          `
+        })
+        salasTemplate+='</div>'
+        document.getElementById("salas").innerHTML = salasTemplate
+        salas.forEach(sala => {
+          mostrarLaboratorios(sala.CAM_CODIGO,sala.LAB_ABREVIATURA)
+        })
+    })
+  connection.end() 
+  
 }
 
 function reloj(){
-    if (!document.layers&&!document.all&&!document.getElementById)
+  if(hora_actual=="07:00:00" || hora_actual=="09:30:00" || hora_actual=="12:00:00" || hora_actual=="14:00:00"){
+    listarControles(obtenerHoraLaboral())
+  } 
+  if (!document.layers&&!document.all&&!document.getElementById)
     return
     
      const Digital=new Date()
@@ -370,7 +184,6 @@ function saberDia(horario){
     document.getElementById("dia").innerHTML = diaTemplate
 }
 function obtenerHoraLaboral(){
-  
     var hora="No es Hora Laboral";
     if(hora_actual.valueOf()>=("06:45:00").valueOf() && hora_actual.valueOf()<("09:15:00").valueOf()){
         hora="07:00:00-09:00:00"
@@ -393,46 +206,11 @@ function obtenerHoraLaboral(){
 function menubar(){
   const templateMenu = [
     {
-      label: '7:00-9:00',
+      label: 'Acerca de:',
       click() {
-        if(hora_actual.valueOf()>="06:45:00"){
-          listarControles("07:00:00-9:00:00");
-        }else{
-          alert("No puede acceder a controles futuros");
-        }
+          alert("Sistema desarrollado por: \nDaniel Lopez rodridani439@gmail.com \nJipson Murillo jymurillo@espe.edu.ec");
       }
-    },
-    {
-      label: '9:30-11:30',
-      click() {
-        if(hora_actual.valueOf()>="09:15:00"){
-          listarControles("09:30:00-11:30:00");
-        }else{
-          alert("No puede acceder a controles futuros");
-        }
-      }
-    },
-    {
-      label: '12:00-14:00',
-      click() {
-        if(hora_actual.valueOf()>="11:45:00"){
-          listarControles("12:00:00-14:00:00");
-        }else{
-          alert("No puede acceder a controles futuros");
-        }
-      }
-    },
-    {
-      label: '14:00-16:00',
-      click() {
-        if(hora_actual.valueOf()>="14:00:00"){
-          listarControles("14:00:00-16:00:00");
-        }else{
-          alert("No puede acceder a controles futuros");
-        }
-      },
-      
-    },  
+    }, 
     
   ];
     // Menu
@@ -440,6 +218,14 @@ function menubar(){
     // Set The Menu to the Main Window
     index.Menu.setApplicationMenu(mainMenu);
     // Menu Template
+}
+
+function seleccionarHora(opcion_hora){
+  if(hora_actual.valueOf()>=opcion_hora){
+    listarControles(opcion_hora);
+  }else{
+    alert("No puede acceder a controles futuros");
+  }
 }
 
 function crearVentanaAutentificar(docente,accion) {
@@ -470,11 +256,34 @@ function crearVentanaAutentificar(docente,accion) {
   }
   
 }
-function mostrarLaboratorios(id){
-  if( document.getElementById(id).checked==false)
-    document.getElementById(id.split("C-",2)[1]).style.display = "none"
-  if( document.getElementById(id).checked==true)
-    document.getElementById(id.split("C-",2)[1]).style.display = "block"
+
+function mostrarLaboratorios(campus,sala){
+  var mostrar=""
+  if( document.getElementById("CAMPUS-"+campus).checked==false){
+    mostrar="none"
+  }
+  if( document.getElementById("CAMPUS-"+campus).checked==true){
+    mostrar="block"
+  }
+  if(sala!=null){
+    document.getElementById(sala).style.display = mostrar
+  }else{
+    const connection = mysql.createConnection(credenciales)
+    let query = 'select LAB_ABREVIATURA FROM laboratorio WHERE LAB_ESTADO=1 AND CAM_CODIGO='+campus
+    connection.connect() 
+    connection.query(query,function(err,rows,fields){
+        if (err){
+          console.log("error fatal")
+          console.log(err)
+          return
+        }
+        let salas=rows;
+        salas.forEach(lab => {
+          document.getElementById(lab.LAB_ABREVIATURA).style.display = mostrar
+        })
+      })
+    connection,end()
+  }
 }
 
 function iniciar(){
